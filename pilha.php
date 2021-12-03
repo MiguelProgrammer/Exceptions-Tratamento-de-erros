15dcff6485964337b1f206b60c165339a3974df7
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-12-02 21:25:09 -0300

    Pegando múltiplas exceções na pilha

--- pilha.php
+++ pilha.php
@@ -5,8 +5,10 @@ function funcao1()
     echo 'Entrei na função 1' . PHP_EOL;
     try{
         funcao2();
-    } catch (RuntimeException $problema){
-        echo "Resolvido o problema da funcao2().".PHP_EOL;
+    } catch (RuntimeException | DivisionByZeroError $problema){
+        echo $problema->getMessage().PHP_EOL;
+        echo $problema->getLine().PHP_EOL;
+        echo $problema->getTraceAsString().PHP_EOL;
     }
     echo 'Saindo da função 1' . PHP_EOL;
 }
@@ -15,15 +17,15 @@ function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
     /*
+     * 1* Caso
+     */
+    $divisao = intdiv(5,0);
+
+    /*
      * 2* Caso
      */
     $arrayFixos = new SplFixedArray(2);
     $arrayFixos[3] = 7;
-    
-    /*
-     * 1* Caso
-     */
-    //$divisao = intdiv(5,0);
 
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
